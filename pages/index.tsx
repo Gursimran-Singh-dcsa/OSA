984ecbbe6dd7932a33d043f927e02a6848be642e
Gursimran Singh <39188119+Gursimran-Singh-dcsa@users.noreply.github.com>
2022-08-19 17:15:55 +0530

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ import KpiComplianceAndTopRetailers from "../components/KpiComplianceAndTopRetai
 import OSAMonthlyAndWeeklyStats from "../components/OSAMonthlyAndWeeklyStats";
 import OverviewRootCauseChart from "../components/OverviewRootCauseChart";
 import styles from '../styles/Home.module.css';
-import EddgieAllStars from "../components/eddgieAllStars";
+import EddgieAllStars from "../components/EddgieAllStars";
 import { EXECUTIVE_SUMMARY_LABEL } from "../store/config";
 
 const Home: NextPage = () => {
@@ -21,15 +21,15 @@ const Home: NextPage = () => {
       <Overview />
       <FilterSection />
       <Kpi />
-      <Row  className={styles.executive_summary_wrapper}><Col xs={24} className={styles.executive_summary_label}>{EXECUTIVE_SUMMARY_LABEL}</Col></Row>
+      <Row style={{marginTop: '1.5rem'}} className={styles.executive_summary_wrapper}><Col xs={24} className={styles.executive_summary_label}>{EXECUTIVE_SUMMARY_LABEL}</Col></Row>
       <OSAMonthlyAndWeeklyStats />
       <KpiComplianceAndTopRetailers/>
       <ExecuitveSummary/>
-      <Row style={{marginTop: '1.5rem'}} justify='center' gutter={[8, 8]}>
+      <Row style={{marginTop: '1.5rem'}} justify='center' gutter={[16, 8]}>
         <Col xs={24} md={24} lg={12} >
           <OverviewRootCauseChart />
         </Col>
-        <Col  xs={24} md={24} lg={12} style={{maxHeight: '346px'}}>
+        <Col  xs={24} md={24} lg={12} >
           <EddgieAllStars />
         </Col>
       </Row>
